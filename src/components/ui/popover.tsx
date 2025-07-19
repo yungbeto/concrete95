8a58dac03db0a58b241fa13630c727dfcc96e539
Roby Saavedra <roby.saavedra@gmail.com>
2025-07-19 04:43:26 +0000

    the slider container still doesn't look like a 90s ui

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -19,7 +20,7 @@ const PopoverContent = React.forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 w-72 rounded-sm border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "z-50 w-48 rounded-none border-2 border-t-white border-l-white border-r-neutral-500 border-b-neutral-500 bg-silver p-2 text-black outline-none",
         className
       )}
       {...props}
